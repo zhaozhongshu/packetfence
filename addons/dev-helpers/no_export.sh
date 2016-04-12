8555892ff45097674b5eb372eca895c4b6c22ec5
James Rouzier <jrouzier@inverse.ca>
2016-04-11 20:55:06 -0400

    Remove some if and assignments

--- addons/dev-helpers/no_export.sh
+++ addons/dev-helpers/no_export.sh
@@ -17,21 +17,16 @@ for module in $MODULES ;do
     #Create a regex for arrays and hashes
     ALL_EXPORTED_VARS_RX="$(perl -Ilib -M$m -e'print join("|", map { $a= $_; $a=~s/^./\\\$/;($a,$_)  } grep { /^[@%]/ }  @'"$m"'::EXPORT);')"
     ALL_EXPORTED_VARS="$(perl -Ilib -M$m -e'print join("|", map { $a= $_; $a=~s/^./\\\$/;($a,$_)  } grep { /^[@%]/ }  @'"$m"'::EXPORT);')"
-    echo "'$ALL_EXPORTED_VALUES' '$ALL_EXPORTED_VARS'"
+    echo "'$ALL_EXPORTED_SUBS' '$ALL_EXPORTED_VARS'"
 
-    if [ -z "$ALL_EXPORTED_VARS_RX" ] && [ -n "$ALL_EXPORTED_VALUES" ];then
+    if [ -z "$ALL_EXPORTED_VARS_RX" ] && [ -n "$ALL_EXPORTED_SUBS" ];then
         for f in $(ag -l "use $m;"); do 
-            EXPORTED_SUBS=
-            EXPORTED_VALUES=
-                if [ -n "$ALL_EXPORTED_SUBS" ];then
-                    EXPORTED_SUBS=$(ag -s --nocolor --nonumbers  -o "$ALL_EXPORTED_SUBS" $f | sort -u | perl -p -e's/\$/\\\$/;s/^/    /;')
-                fi
-                EXPORTED_VALUES=$EXPORTED_SUBS
-                if [ -n "$EXPORTED_VALUES" ];then
-                    perl -pi -e"s/use $m;/use $m qw(\n$EXPORTED_VALUES\n);/" $f
-                else
-                    perl -pi -e"s/use $m;//" $f
-                fi
+            EXPORTED_VALUES=$(ag -s --nocolor --nonumbers  -o "$ALL_EXPORTED_SUBS" $f | sort -u | perl -p -e's/\$/\\\$/;s/^/    /;')
+            if [ -n "$EXPORTED_VALUES" ];then
+                perl -pi -e"s/use $m;/use $m qw(\n$EXPORTED_VALUES\n);/" $f
+            else
+                perl -pi -e"s/use $m;//" $f
+            fi
         done
         perl -pi -e's/,? *\@EXPORT_OK//' $module
         perl -pi -e's/EXPORT(?!_OK)/EXPORT_OK/' $module
