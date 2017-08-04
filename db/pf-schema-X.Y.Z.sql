637f5d52b25af5134d8d23900645a66d5fe10241
Durand Fabrice <fdurand@inverse.ca>
2017-08-04 09:41:57 -0400

    Moved create radreply table in the correct files

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -1219,6 +1219,20 @@ CREATE TABLE `chi_cache` (
 );
 
 --
+-- Add 'radreply' table
+--
+
+CREATE TABLE radreply (
+  id int(11) unsigned NOT NULL auto_increment,
+  username varchar(64) NOT NULL default '',
+  attribute varchar(64) NOT NULL default '',
+  op char(2) NOT NULL DEFAULT '=',
+  value varchar(253) NOT NULL default '',
+  PRIMARY KEY  (id),
+  KEY username (username(32))
+);
+
+--
 -- Updating to current version
 --
 
